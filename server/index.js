286cf71ca70633f0bfb90b7eee5125334c979697
Santana <mohamed.boufelfel@epitech.eu>
2024-07-08 17:30:04 +0200

    push santana

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"]
   }
 });
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
   socket.on('setUsername', (username) => {
     if (!users[socket.id]) {
       users[socket.id] = username;
-      io.emit('message', { user: 'Bot ', text : `${username} est arrivé sur le serveur` });
+      io.emit('message', { user: 'Bot', text : `${username} est arrivé sur le serveur` });
     }
   });
 
@@ -28,12 +28,12 @@ io.on('connection', (socket) => {
     rooms[roomName] = roomName;
     socket.join(roomName);
     io.emit('rooms', Object.keys(rooms));
-    io.to(roomName).emit('message', { user: 'Bot ', text : `${users[socket.id]} a créé le salon ${roomName}` });
+    io.to(roomName).emit('message', { user: 'Bot', text : `${users[socket.id]} a créé le salon ${roomName}` });
   });
 
   socket.on('joinRoom', (roomName) => {
     socket.join(roomName);
-    io.to(roomName).emit('message', { user: 'Bot ', text : `${users[socket.id]} a rejoint le salon ${roomName}` });
+    io.to(roomName).emit('message', { user: 'Bot', text : `${users[socket.id]} a rejoint le salon ${roomName}` });
   });
 
   socket.on('message', (message) => {
@@ -42,7 +42,7 @@ io.on('connection', (socket) => {
       const newNick = message.text.split(' ')[1];
       const oldNick = users[socket.id];
       users[socket.id] = newNick;
-      io.emit('message', { user: 'Bot ', text : `${oldNick} a changé son pseudo en ${newNick}` });
+      io.emit('message', { user: 'Bot', text : `${oldNick} a changé son pseudo en ${newNick}` });
     } else {
       const roomName = message.roomName;
       io.to(roomName).emit('message', { user: user, text : message.text });
@@ -51,10 +51,9 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     const username = users[socket.id];
-    if (!users[socket.id]) {
-    io.emit('message', { user: 'Bot ', text : `${username} a quitté le serveur` });
+    console.log(`User disconnected: ${username}`);
+    io.emit('message', { user: 'Bot', text : `${username} a quitté le serveur` });
     delete users[socket.id];
-    }
   });
 });
 
